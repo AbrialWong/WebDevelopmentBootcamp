062409efef780211d5989ed64111caa7edf054de
Abrial Wong <abrial-wong@live.com.sg>
2021-02-21 02:31:56 +0800

    projects

---
+++ Projects/EJS/todolist-v1/app.js
@@ -0,0 +1,85 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = []; 
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true})); // tell our app to use bodyParser
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+const day = date.getDate();
+    
+   // var currentDay = today.getDay();
+    // var day ="";
+    
+    // if(currentDay == 6 || currentDay == 0 ){ // check if the day is sat or sun       
+    //     day = "Weekend";
+    // }else{
+    //     day = "Weekday";     
+    // }
+
+    // switch(currentDay){
+    //     case 0:
+    //         day = "Sunday";
+    //     break;
+    //     case 1:
+    //         day = "Monday";
+    //     break;
+    //     case 2:
+    //         day = "Tuesday";
+    //     break;
+    //     case 3:
+    //         day = "Wednesday";
+    //     break;
+    //     case 4:
+    //         day = "Thursday";
+    //     break;
+    //     case 5:
+    //         day = "Friday";
+    //     break;
+    //     case 6:
+    //         day = "Saturday";
+    //     break;
+    //     default:
+    //         console.log("Error, Currently day is equals to " + currentDay);
+    //}
+    res.render("list", {listTitle:day, newListItems:items});
+});
+
+app.post("/", function(req, res){
+
+    const item = req.body.newItem;
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else{   
+    items.push(item);
+
+    res.redirect("/");
+    }
+});
+
+app.get("/work", function(req,res){
+res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req,res){
+const item = req.body.newItem;
+workItems.push(item);
+res.redirect("/work");
+});
+
+app.get("/about", function(req,res){
+res.render("about");
+});
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});

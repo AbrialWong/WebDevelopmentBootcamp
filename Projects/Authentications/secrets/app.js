c381e89d5b8c8c0b11dee81bbf835e4f5223ce57
Abrial Wong <abrial-wong@live.com.sg>
2021-03-01 14:47:52 +0800

    Complete Database Encryptions

--- Projects/Authentications/secrets/app.js
+++ Projects/Authentications/secrets/app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-// Task: To Register Users with Username and Password
+// Task: Database Encryption
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(express.static("public"));
@@ -15,10 +16,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema( {
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt,{secret:secret , encryptedFields:["password"]}); // adding the encryption plugin for the schema
 
 const User = new mongoose.model("User", userSchema);
 

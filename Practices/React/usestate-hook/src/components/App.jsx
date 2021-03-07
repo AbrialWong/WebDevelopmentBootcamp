b59029f95804cf5488bd2c89f9d4ff34a89728e8
Abrial Wong <abrial-wong@live.com.sg>
2021-03-07 23:34:36 +0800

    use hook

---
+++ Practices/React/usestate-hook/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function App(){
+const state = useState(123);
+
+console.log(state[0]);
+
+function increase (){
+  //count ++;
+}
+
+return (
+  <div className="container">
+    <h1>{state[0]}</h1>
+    <button onClick={increase}>+</button>
+  </div>,
+  document.getElementById("root")
+);
+
+}
+
+export default App;
